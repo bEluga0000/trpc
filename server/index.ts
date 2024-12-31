b61c90ffc59d51b33d21725c73533d5ba42bb8d8
shreyas20003 <shreyasas2975@gmail.com>
2024-12-31 22:05:14 +0530

    Serving API - standalone

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import { publicProcedure, router } from './trpc';
 import {z} from "zod";
-// import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
 const createTodoInputSchema = z.object({
     title : z.string(),
     description : z.string()
@@ -24,8 +24,8 @@ const appRouter = router({
 });
 
 export type AppRouter = typeof appRouter;
-// const server = createHTTPServer({
-//     router: appRouter,
-// });
+const server = createHTTPServer({
+    router: appRouter,
+});
 
-// server.listen(3000);
+server.listen(3000);
