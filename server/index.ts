12ac9628e1dd178ac086d51602bde148c3e38b59
shreyas20003 <shreyasas2975@gmail.com>
2024-12-31 22:03:54 +0530

    writing procedures

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,31 @@
 import { publicProcedure, router } from './trpc';
-
+import {z} from "zod";
+// import { createHTTPServer } from '@trpc/server/adapters/standalone';
+const createTodoInputSchema = z.object({
+    title : z.string(),
+    description : z.string()
+})
 const appRouter = router({
-    // createTodo:publicProcedure
-    // .input()
+    createTodo:publicProcedure
+    .input(createTodoInputSchema)
+    .mutation(async (opts)=>{
+        const title = opts.input.title
+        const desc = opts.input.description
+        // db operation is going to be done here
+        return {id:"1"}
+    }),
+    getTodo:publicProcedure
+    .input(z.object({id:z.string()}))
+    .query(async (opts)=>{
+        const id = opts.input.id
+        // db operation
+        return {id:"1",title:"good",description:"Nicely done"}
+    })
 });
 
 export type AppRouter = typeof appRouter;
+// const server = createHTTPServer({
+//     router: appRouter,
+// });
+
+// server.listen(3000);
